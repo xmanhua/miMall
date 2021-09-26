68db849c09b4277fc92ac5b78126b959e50b2d9b
manhua xiao <manhuax@ciandt.com>
2021-09-26 10:21:34 +0800

    update publicPath

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # miMall
 一个 Vue.js 练手项目
-  [demo](https://xmanhua.github.io/miMall/dist/index.html) 
+  [demo](https://xmanhua.github.io/miMall/dist) 
---
+++ vue.config.js
@@ -0,0 +1,23 @@
+module.exports = {//https://cli.vuejs.org/zh/config/#pages
+    // lintOnSave: false,// 开发环境下每次保存代码时都启用 eslint验证
+    devServer: {
+        host: 'localhost',
+        port: 8080,
+        proxy: {//代理
+            '/api': {
+                target: 'http://mall-pre.springboot.cn',
+                changOrigin: true,//是否将主机头设为原点
+                pathRewrite: {
+                    '/api' :  '',//把api设为空，api后面的路径与主机相拼，不用单个配
+                }
+            }
+        }
+    },
+    chainWebpack:(config)=>{
+        config.plugins.delete('prefetch');
+    },
+    publicPath: '/miMall/dist',//子路径名,github用
+    // outputDir: 'dist',//不写也一样，默认就是dist
+    // indexPath: 'index2.html',//生成首页
+    // productionSourceMap: false,// 不生成map文件，不写也一样，默认不生成map
+}
